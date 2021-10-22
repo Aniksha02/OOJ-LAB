60888b6aaaad3638ac8b7588b31d51b880a34b9a
ANIKSHA S <84078705+Aniksha02@users.noreply.github.com>
2021-10-22 12:30:48 +0530

    Create Example 2.java

---
+++ Example 2.java
@@ -0,0 +1,10 @@
+class example 2{
+ public static void main (string args[]){
+ 
+int num;
+num =100;
+system out println("this is a number.");
+num=num*2
+system out print("the value of num*2.");
+system.out.println(num);
+}}
