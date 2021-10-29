023367b60b2534267efed62a29afec117ea75135
ANIKSHA S <84078705+Aniksha02@users.noreply.github.com>
2021-10-29 12:37:28 +0530

    Create rowsnotright.java

---
+++ rowsnotright.java
@@ -0,0 +1,29 @@
+import java.util;
+import java.util.scanner;
+
+class rows{
+
+void pattern(int n)
+{
+int i,j,c=1,n;
+for(i=0,i<n,i++);
+{
+for(j=0;j<=i;j++);
+{
+System.out.print(c+"");
+c++;
+}
+System.out.println();
+}}
+
+public static void main(String.args[])
+{
+
+rows obj=new rows();
+Scanner sc=new Scanner(System.in);
+system.out.print("enter number of rows");
+int n =sc.nextInt();
+}
+}
+
+
